8d92fdcd58976e40135501df6ff904af966196b7
Lorran Moncada Mendes <lorran.mendes@datum.inf.br>
2021-08-26 21:18:46 -0300

    comitando projeto gateway

---
+++ Service1/ApiServiceOne/Controllers/WeatherForecastController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiServiceOne.Controllers
+{
+    [ApiController]
+    [Route("api/v1")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("weather-forecast/{id:int}")]
+        public IEnumerable<WeatherForecast> Get(int id)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Id = id,
+                NomeApi = "API 1",
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        [Route("user")]
+        public IActionResult Cadastrar([FromBody] User Usuario)
+        {
+            return Ok(Usuario);
+        }
+
+
+    }
+
+    public class User
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
+}
